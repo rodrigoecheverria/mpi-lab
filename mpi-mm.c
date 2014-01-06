597cef0b15cdd753be778b93adcbd9ab06f8458d
Rodrigo Echeverria <rodrigoecheverria@outlook.com>
2014-01-06 22:57:03 +0100

    working

--- mpi-mm.c
+++ mpi-mm.c
@@ -4,139 +4,160 @@
 
 #define TAG 13
 
+void PrintMatrix(double **Mat, int N, int M);
+
 int main(int argc, char *argv[]) {
   double **A, **B, **C, *tmp;
-  double computationTime = 0.0, totalComputationTime = 0.0, checksum = 0.0, applicationTime = 0.0, startComputationTime = 0.0, endComputationTime = 0.0; 
-  int numElements, offset, stripSize, myrank, numnodes, N, i, j, k;
+  double computationTime = 0.0, totalComputationTime = 0.0, checksum = 0.0, 
+         applicationTime = 0.0, startComputationTime = 0.0, endComputationTime = 0.0; 
+  int numElements, offset, stripSize, myrank, numnodes, N, M, i, j, k;
   
+// Initialization =================================================================== 
   MPI_Init(&argc, &argv);
-  
+ 
   MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
   MPI_Comm_size(MPI_COMM_WORLD, &numnodes);
-  if (myrank == 0)
-	applicationTime = MPI_Wtime();
+
+  if (myrank == 0) applicationTime = MPI_Wtime();
+    
   N = atoi(argv[1]);
+  M = atoi(argv[2]);
   
-  // allocate A, B, and C --- note that you want these to be
-  // contiguously allocated.  Workers need less memory allocated.
+  stripSize = M / numnodes; //Quantity of work done per node (divide A row-wise)
   
+// Memory allocation ================================================================
+//   allocate A, B, and C --- note that you want these to be
+//   contiguously allocated.  Workers need less memory allocated.
+// ==================================================================================
+// A:
   if (myrank == 0) {
-    tmp = (double *) malloc (sizeof(double ) * N * N);
-    A = (double **) malloc (sizeof(double *) * N);
-    for (i = 0; i < N; i++)
+    tmp = (double *) malloc (sizeof(double ) * N * M);
+    A = (double **) malloc (sizeof(double *) * M);
+    for (i = 0; i < M; i++)
       A[i] = &tmp[i * N];
-  }
-  else {
-    tmp = (double *) malloc (sizeof(double ) * N * N / numnodes);
-    A = (double **) malloc (sizeof(double *) * N / numnodes);
-    for (i = 0; i < N / numnodes; i++)
+  } else {
+    tmp = (double *) malloc (sizeof(double ) * N * stripSize);
+    A = (double **) malloc (sizeof(double *) * stripSize);
+    for (i = 0; i < stripSize; i++)
       A[i] = &tmp[i * N];
   }
   
-  
-  tmp = (double *) malloc (sizeof(double ) * N * N);
-  B = (double **) malloc (sizeof(double *) * N);
-  for (i = 0; i < N; i++)
+// B:
+  tmp = (double *) malloc (sizeof(double ) * N * M);
+  B = (double **) malloc (sizeof(double *) * M);
+  for (i = 0; i < M; i++)
     B[i] = &tmp[i * N];
   
-  
+// C:
   if (myrank == 0) {
-    tmp = (double *) malloc (sizeof(double ) * N * N);
-    C = (double **) malloc (sizeof(double *) * N);
-    for (i = 0; i < N; i++)
-      C[i] = &tmp[i * N];
-  }
-  else {
-    tmp = (double *) malloc (sizeof(double ) * N * N / numnodes);
-    C = (double **) malloc (sizeof(double *) * N / numnodes);
-    for (i = 0; i < N / numnodes; i++)
-      C[i] = &tmp[i * N];
+    tmp = (double *) malloc (sizeof(double ) * M * M);
+    C = (double **) malloc (sizeof(double *) * M);
+    for (i = 0; i < M; i++)
+      C[i] = &tmp[i * M];
+  } else {
+    tmp = (double *) malloc (sizeof(double ) * M * stripSize);
+    C = (double **) malloc (sizeof(double *) * stripSize);
+    for (i = 0; i < stripSize; i++)
+      C[i] = &tmp[i * M];
   }
 
+// Matrix initialization (Random) ===================================================
   if (myrank == 0) {
     srand(1);
-    // initialize A and B
-    for (i=0; i<N; i++) {
-      for (j=0; j<N; j++) {
+    for (i = 0; i < M; i++) 
+      for (j = 0; j < N; j++) {
         A[i][j] = 0.5 + (double)rand()/RAND_MAX;
         B[i][j] = 0.5 + (double)rand()/RAND_MAX;
       }
-    }
   }
   
-  stripSize = N/numnodes;
-
-  // send each node its piece of A -- note could be done via MPI_Scatter
+// Send initial data ================================================================
+// A:
   if (myrank == 0) {
     offset = stripSize;
     numElements = stripSize * N;
-    for (i=1; i<numnodes; i++) {
+    for (i = 1; i < numnodes; i++) {
       MPI_Send(A[offset], numElements, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD);
       offset += stripSize;
     }
-  }
-  else {  // receive my part of A   
+  } else {
     MPI_Recv(A[0], stripSize * N, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
   }
   
-  // everyone gets B
-  MPI_Bcast(B[0], N*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+// B:
+  MPI_Bcast(B[0], N * M, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-  // Let each process initialize C to zero 
-  for (i=0; i<stripSize; i++) {
-    for (j=0; j<N; j++) {
-      C[i][j] = 0.0;
-    }
-  }
 
-  // do the work
-  // start timer =========================================================================================================
+// Computation ======================================================================
   startComputationTime = MPI_Wtime();
+ 
+// Initialize C to 0
+  for (i = 0; i < stripSize; i++)
+    for (j = 0; j < N; j++)
+      C[i][j] = 0.0;
 
-  for (i=0; i<stripSize; i++)
-    for (j=0; j<N; j++)
-      for (k=0; k<N; k++)
-	C[i][j] += A[i][k] * B[k][j];
+// Compute my C elements
+  for (i = 0; i < stripSize; i++)
+    for (j = 0; j < M; j++)
+      for (k = 0; k < N; k++)
+	      C[i][j] += A[i][k] * B[j][k]; //B is transposed
 
-  // stop timer =========================================================================================================
+// Stop timer
   endComputationTime = MPI_Wtime();
   computationTime += endComputationTime - startComputationTime;
+// Computation End ==================================================================
 
-  // master receives from workers  -- note could be done via MPI_Gather
+//Receive Computed Data =============================================================
   if (myrank == 0) {
     offset = stripSize; 
-    numElements = stripSize * N;
-    for (i=1; i<numnodes; i++) {
-      MPI_Recv(C[offset], numElements, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    numElements = stripSize * M;
+    for (i = 1; i < numnodes; i++) {
+      MPI_Recv(C[offset], numElements, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD, 
+                MPI_STATUS_IGNORE);
       offset += stripSize;
     }
   }
-  else { // send my contribution to C
-    MPI_Send(C[0], stripSize * N, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD);
+  else { 
+    MPI_Send(C[0], stripSize * M, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD);
   }
   
-  MPI_Reduce(&computationTime, &totalComputationTime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+// Add up the computation time of all nodes ========================================= 
+  MPI_Reduce(&computationTime, &totalComputationTime, 1, MPI_DOUBLE, MPI_SUM, 0, 
+              MPI_COMM_WORLD);
   
-  
-  // print out matrix here, if I'm the master
+// Calculate the checksum ===========================================================
   if (myrank == 0) {
-    for (i=0; i<N; i++) 
-      for (j=0; j<N; j++) 
+    for (i=0; i<M; i++) 
+      for (j=0; j<M; j++)
         checksum += C[i][j];
   }
+  
+  if (myrank == 0) {
+	  applicationTime = MPI_Wtime() - applicationTime;
+    //PrintMatrix(C,M,M);
+	  printf("Checksum: %f, Application time: %f, Total time: %f\n",checksum, 
+	          applicationTime, totalComputationTime);
+  }
+    
   free(*A);
   free(A);
   free(*B);
   free(B);
   free(*C);
   free(C);
-  if (myrank == 0)
-  {
-	applicationTime = MPI_Wtime() - applicationTime;
-	printf("Checksum: %e, Application time: %f, Total time: %f\n",checksum, applicationTime, totalComputationTime);
-  }
   MPI_Finalize();
   return 0;
 }
 
+void PrintMatrix(double **Mat, int N, int M)
+{
+  if (N < 20 && M < 20) {
+    int i,j;
+    for (i=0; i<N; i++){
+      for (j=0; j<M; j++)
+        printf("%f,",Mat[i][j]);
+      printf("\n");
+    }
+  }
+}
 
--- mpijac.c
+++ mpijac.c
@@ -6,24 +6,27 @@
 
 #define TAG 13
 void logIfMaster(int rank, const char * format, ...);
+void PrintVector(double *Vec, int N);
 
 int main(int argc, char *argv[]) {
-  double *Ag, **A, *b, *bg, *x1, *x2, *last, *current, *tmp;
-  double computationTime = 0.0, totalComputationTime = 0.0, checksum = 0.0, applicationTime = 0.0, startComputationTime = 0.0, endComputationTime = 0.0, sum_a, max_diff, local_max_diff, diff, epsilon; 
+  double *Ag, **A, *b, *bg, *x1, *x2, *last, *current, *tmp,
+         computationTime = 0.0, totalComputationTime = 0.0, applicationTime = 0.0,
+         startComputationTime = 0.0, endComputationTime = 0.0, 
+         sum_a, max_diff, local_max_diff, diff, epsilon, checksum = 0.0,
+         alfa=0.5,error=0.0;
   int numElements, offset, stripSize, myrank, numnodes, c, N, i, j, k, it_count;
-  double alfa=0.5;
-  double error=0.0;
   MPI_Datatype strip;
 
+// Initialization ===================================================================
   MPI_Init(&argc, &argv);
   
   MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
   MPI_Comm_size(MPI_COMM_WORLD, &numnodes);
 
-  if (myrank == 0)
-	  applicationTime = MPI_Wtime();
-  c = atoi(argv[1]);
-  N = (c>0) ? c * 1024 : 4;
+  if (myrank == 0) applicationTime = MPI_Wtime();
+  
+  c = atoi(argv[1]); 
+  N = (c > 0) ? c * 1024 : 4; //If user inputs a 0, will use a 4x4 test case
   epsilon = pow(10, -(atoi(argv[2])));
 
   it_count = 0;
@@ -36,12 +39,13 @@ int main(int argc, char *argv[]) {
     printf("Could not open file for process: %d\n",myrank);
     exit(1);
   }*/
-//Strip calculation ===================================================================
+  
+//Strip calculation =================================================================
   stripSize = N/numnodes;
   MPI_Type_vector(stripSize, N, N, MPI_DOUBLE, &strip);
   MPI_Type_commit(&strip); 
     
-//Memory allocation ===================================================================
+//Memory allocation =================================================================
   if (myrank == 0) {
     Ag = (double *) malloc (sizeof(double) * N * N);
     bg = (double *) malloc (sizeof(double) * N);
@@ -57,26 +61,25 @@ int main(int argc, char *argv[]) {
   
 //Initialize matrixes  =================================================================
   if (myrank == 0) {
-   if (c > 0) {
-    srand(1);
-    // initialize Ag, bg & x to random values
-    for (i=0; i<N; i++) {
-      for (j=0; j<N; j++) {
-        Ag[i*N + j] = 0.5 + (double)rand()/RAND_MAX;
+    if (c > 0) {
+      //Initialize Ag, bg & x to random values
+      srand(1);
+      for (i=0; i<N; i++) {
+        for (j=0; j<N; j++) {
+          Ag[i*N + j] = 0.5 + (double)rand()/RAND_MAX;
+        }
+        bg[i] = 0.5 + (double)rand()/RAND_MAX;
+        x1[i] = bg[i];
       }
-      bg[i] = 0.5 + (double)rand()/RAND_MAX;
-      x1[i] = bg[i];
-    }
     
-    //Enforce diagonal dominancy in Ag
-    for (i=0; i<N; i++) {
-      Ag[i*N + j] = 0.0;
-      for (j=0; j<N; j++) {
-        Ag[i*N + i]+=Ag[i*N + j];
+      //Enforce diagonal dominancy in Ag
+      for (i=0; i<N; i++) {
+        Ag[i*N + j] = 0.0;
+        for (j=0; j<N; j++) {
+          Ag[i*N + i]+=Ag[i*N + j];
+        }
       }
-    }
-
-  } else { // Test case
+    } else { // Test case
       Ag[0] = 2.0;  Ag[1] = 1.0; Ag[2] = 5.0;  Ag[3] = 7.0;
       Ag[4] = 1.0;  Ag[5] = -4.0; Ag[6] = 2.0;  Ag[7] = 1.0;
       Ag[8] = -1.0; Ag[9] = 2.0;  Ag[10] = 4.0; Ag[11] = 1.0;
@@ -89,75 +92,75 @@ int main(int argc, char *argv[]) {
     }
   }
        
-//Send initial data  ===================================================================
+//Send initial data  ================================================================
   MPI_Scatter(Ag, 1, strip, &(A[0][0]), 1, strip, 0, MPI_COMM_WORLD);
-  MPI_Scatter(bg, stripSize, MPI_DOUBLE, &(b[0]), stripSize, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Scatter(bg, stripSize, MPI_DOUBLE, &(b[0]), stripSize, MPI_DOUBLE, 0, 
+                MPI_COMM_WORLD);
   MPI_Bcast(x1,N,MPI_DOUBLE,0,MPI_COMM_WORLD);
   
-//Jacobi iterations  ===================================================================
-  //ping-pong buffer between x1 and x2 initialition
+//Jacobi iterations  ================================================================
+  //ping-pong buffer between x1 and x2 initialization
   last = x1;
   current = x2;
 
   do {
-    //Main computation
+    //Main computation ==============================================================
     startComputationTime = MPI_Wtime();
+    
     offset = stripSize * myrank;
     for (i=0; i<stripSize; i++) {
       sum_a = 0.0;
-      for (j=0; j < offset + i; j++){
+      for (j=0; j < offset + i; j++)
         sum_a += A[i][j]*last[j];
-      }
-      for (j=offset+i+1; j<N; j++){
+      
+      for (j=offset+i+1; j<N; j++)
         sum_a += A[i][j]*last[j]; 
-      }
-      current[offset+i] =alfa* (b[i] - sum_a)/A[i][offset + i]+(1-alfa)*last[offset+i];
+      
+      current[offset+i] = alfa * (b[i] - sum_a)/A[i][offset + i] +
+                          (1 - alfa) * last[offset + i];
     }
     
     endComputationTime = MPI_Wtime();
     computationTime += endComputationTime - startComputationTime;   
+    //End of main computation =======================================================
 
-    //End of main computation
-
-    MPI_Gather(&current[offset], stripSize, MPI_DOUBLE, current, stripSize, MPI_DOUBLE, 0,MPI_COMM_WORLD);
+    // Get the new solution & redistribute it
+    MPI_Gather(&current[offset], stripSize, MPI_DOUBLE, current, stripSize, 
+                MPI_DOUBLE, 0,MPI_COMM_WORLD);
     MPI_Bcast(current,N,MPI_DOUBLE,0,MPI_COMM_WORLD);
 
-    //Calculate my local max diff
-
+    //Calculate my local max difference
     local_max_diff = fabs(1 - last[0]/current[0]);
-
     diff = 0.0;
     for (i=1; i<stripSize; i++) {
       diff = fabs(1- last[i]/current[i]);      
       if (diff > local_max_diff) local_max_diff = diff;
     }
     
-    //interchange x-buffers
+    //Interchange x-buffers
     tmp = last;
     last = current;
     current = tmp;
-
-    it_count++;
     
-    //calculate convergence : max (local_max_errors) > epsilon
-
+    //Calculate convergence : max_diff = max (local_max_errors) > epsilon
     MPI_Allreduce(&local_max_diff, &max_diff, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
-    if(myrank==0) printf ("Iteration: %d, current: %e, diff: %e\n",it_count,last[0],max_diff);
 
   } while (max_diff > epsilon); 
+//End of Jacobi iterations  =========================================================
 
-  MPI_Barrier(MPI_COMM_WORLD);
+  //MPI_Barrier(MPI_COMM_WORLD);
   MPI_Reduce(&computationTime, &totalComputationTime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
   if (myrank == 0)
-    for (i = 0; i < N; i++)
-      printf("x[%d]=%f\n",i,last[i]);
+    PrintVector(current, N);
       
   if (myrank == 0) {
     for (i=0; i<N; i++) 
         checksum += last[i];
+        
 	applicationTime = MPI_Wtime() - applicationTime;
-	printf("Checksum: %e, Application time: %f, Total time: %f\n",checksum, applicationTime, totalComputationTime);
+	printf("Checksum: %e, Application time: %f, Total time: %f\n",checksum, 
+	        applicationTime, totalComputationTime);
   }
 
 
@@ -184,4 +187,13 @@ void logIfMaster(int rank, const char * format, ...)
   if (rank == 0) vprintf(format, arglist);
 }
 
-
+void PrintVector(double *Vec, int N)
+{
+  if (N < 20) {
+    int i;
+    printf ("x = (");
+    for (i = 0; i < N - 1; i++)
+      printf("%f, ", Vec[i]);
+    printf("%f)\n", Vec[N-1]); 
+  }
+}
