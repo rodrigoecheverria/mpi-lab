0e68082647ed99ffbde3614138643f6ec83b0fed
Rodrigo Echeverria Correa <rodrigoecheverria@outlook.com>
2014-01-22 22:46:36 +0100

    Update scriptjac1.sh

--- scriptjac1.sh
+++ scriptjac1.sh
@@ -3,7 +3,7 @@
 #$ -q aolin.q
 #$ -v SGE_QMASTER_PORT
 #$ -cwd
-#$ -l h_rt=60
+#$ -l h_rt=600
 module load openmpi/1.6.3
 mpirun -np 4 $HOME/code/mpilab2/jac.o 1 2
 mpirun -np 4 $HOME/code/mpilab2/jac.o 1 4
