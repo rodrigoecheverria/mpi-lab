fac7fc224ebccb526ac708806da7446ebff4e1b1
Rodrigo Echeverria Correa <rodrigoecheverria@outlook.com>
2014-01-30 12:55:43 +0100

    Update scriptjac_4_24.sh

--- scriptjac_4_24.sh
+++ scriptjac_4_24.sh
@@ -5,9 +5,13 @@
 #$ -cwd
 #$ -l h_rt=3600
 module load openmpi/1.6.3
+echo "mpirun -np 4 $HOME/code/mpilab2/jac.o 24 2"
 mpirun -np 4 $HOME/code/mpilab2/jac.o 24 2
+echo "mpirun -np 4 $HOME/code/mpilab2/jac.o 24 4"
 mpirun -np 4 $HOME/code/mpilab2/jac.o 24 4
+echo "mpirun -np 4 $HOME/code/mpilab2/jac.o 24 8"
 mpirun -np 4 $HOME/code/mpilab2/jac.o 24 8
+echo "mpirun -np 4 $HOME/code/mpilab2/jac.o 24 16"
 mpirun -np 4 $HOME/code/mpilab2/jac.o 24 16
 
 exit 0
